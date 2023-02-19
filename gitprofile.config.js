db8815dc219b82c62bb89d74247a5896f4900ee9
Tony Hollaar <thollaar@gmail.com>
2023-02-19 16:32:49 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -131,6 +131,13 @@ const config = {
       imageUrl: 'https://github.com/tonyhollaar/projects/blob/12a1514c76c25a08b0c03dabd9712fee72e20f33/Visualizations_Bar_Chart_Race_Example.gif?raw=true',
       link: 'https://www.tonyhollaar.com/portfolio/visualizations',
     },
+    {
+      title: 'Visualizations',
+      description:
+        'Use cases for utilizing SQL queries such as aggregatations (count/sum) and flattening of SQL tables with e.g. 1 row per user utilizing Python package SQLite3',
+      imageUrl: 'https://github.com/tonyhollaar/tonyhollaar.github.io/blob/main/img/portfolio_sqlite_logo.png?raw=true',
+      link: 'https://www.tonyhollaar.com/portfolio/visualizations',
+    },
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
