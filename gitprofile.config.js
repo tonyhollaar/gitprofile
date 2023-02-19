0163fe477aa90f4040040c82d89cf3228704c0f1
Tony Hollaar <thollaar@gmail.com>
2023-02-19 14:52:58 -0500

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -147,7 +147,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'procyon',
+    defaultTheme: 'lofi',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
